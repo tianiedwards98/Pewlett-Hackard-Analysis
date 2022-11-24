d6e69b3f92f75aa2b6fd8e37c7c8eab976e83de2
Tiani Edwards <tianiedwards@Tianis-MacBook-Pro.local>
2022-11-23 21:14:17 -0600

    Adding quereis to files

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,1 +1,49 @@
+--Deliverable 1 Pt.1
+Select e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+Into retirement_titles
+From employees as e
+Inner Join titles As t
+On(e.emp_no = t.emp_no)
+Where (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+Order by emp_no Asc;
 
+--Pt 2
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+INTO unique_titles
+FROM retirement_titles As rt
+WHERE (to_date ='9999-01-01' )
+ORDER BY emp_no Asc, to_date DESC;
+
+SELECT COUNT(ut.emp_no),ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+
+--Deliverable 2
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+--DISTINCT ON(emp_no)
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
